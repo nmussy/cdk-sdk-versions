b5e17913aaea4f9d055a3e9f67dfd13c74b0e4e9
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-03-13 08:25:12 +0100

    feat: bedrock runner

--- src/provider/bedrock.ts
+++ src/provider/bedrock.ts
@@ -3,7 +3,9 @@ import {
 	FoundationModelLifecycleStatus,
 	ListFoundationModelsCommand,
 } from "@aws-sdk/client-bedrock";
-import { FoundationModelIdentifier } from "aws-cdk-lib/aws-bedrock";
+import console from "console";
+import { CONSOLE_SYMBOLS } from "../util";
+import { getCDKFoundationModelIdentifiers } from "../util/provider/bedrock";
 
 const client = new BedrockClient({});
 export const getFoundationModels = async () => {
@@ -12,14 +14,55 @@ export const getFoundationModels = async () => {
 	return models.modelSummaries ?? [];
 };
 
-(async () => {
-	const models = await getFoundationModels();
+const runBedrock = async () => {
+	const sdkModels = await getFoundationModels();
+	const cdkModels = getCDKFoundationModelIdentifiers();
 
-	for (const model of models) {
+	for (const cdkModel of cdkModels) {
+		const sdkModel = sdkModels.find(
+			({ modelId }) => modelId === cdkModel.model.modelId,
+		);
+
 		const isDeprecated =
-			model.modelLifecycle?.status !== FoundationModelLifecycleStatus.ACTIVE;
-		console.log(model.modelId, isDeprecated);
-	}
-})();
+			sdkModel?.modelLifecycle?.status !==
+			FoundationModelLifecycleStatus.ACTIVE;
 
-console.log(FoundationModelIdentifier);
+		if (!sdkModel) {
+			if (cdkModel.isDeprecated) continue;
+
+			console.log(CONSOLE_SYMBOLS.DELETE, cdkModel.model.modelId);
+		} else if (!cdkModel.isDeprecated && isDeprecated) {
+			console.log(
+				CONSOLE_SYMBOLS.UPDATE,
+				cdkModel.model.modelId,
+				"@deprecated",
+			);
+		} else if (cdkModel.isDeprecated && !isDeprecated) {
+			console.log(
+				CONSOLE_SYMBOLS.UPDATE,
+				cdkModel.model.modelId,
+				"not @deprecated",
+			);
+		}
+	}
+
+	for (const sdkModel of sdkModels) {
+		const cdkModel = cdkModels.find(
+			({ model: { modelId } }) => modelId === sdkModel.modelId,
+		);
+
+		if (!cdkModel) {
+			const isDeprecated =
+				sdkModel?.modelLifecycle?.status !==
+				FoundationModelLifecycleStatus.ACTIVE;
+
+			console.log(
+				CONSOLE_SYMBOLS.ADD,
+				sdkModel.modelId,
+				isDeprecated ? "@deprecated" : "",
+			);
+		}
+	}
+};
+
+if (process.env.NODE_ENV !== "test") void runBedrock();
---
+++ src/util/provider/bedrock.ts
@@ -0,0 +1,46 @@
+import { FoundationModelIdentifier } from "aws-cdk-lib/aws-bedrock";
+import { CdkLibPath } from "../cdk";
+import { getStaticFieldComments } from "../tsdoc";
+
+export const CDK_BEDROCK_FOUNDATION_MODEL_PATH = new CdkLibPath(
+	"aws-bedrock/lib/foundation-model.d.ts",
+);
+export interface DeprecableFoundationModelIdentifier {
+	model: FoundationModelIdentifier;
+	isDeprecated: boolean;
+}
+
+const bedrockConstructorRegex =
+	/new FoundationModelIdentifier\('(?<modelId>[\w.-]+)'\)/;
+
+export const getCDKFoundationModelIdentifiers = () => {
+	const bedrockModels: DeprecableFoundationModelIdentifier[] = [];
+
+	for (const { fieldName, fieldValue, isDeprecated } of getStaticFieldComments(
+		CDK_BEDROCK_FOUNDATION_MODEL_PATH.auto,
+	)) {
+		let model: FoundationModelIdentifier;
+		if (fieldName in FoundationModelIdentifier) {
+			model = FoundationModelIdentifier[
+				fieldName as keyof typeof FoundationModelIdentifier
+			] as FoundationModelIdentifier;
+		} else {
+			const match = fieldValue.match(bedrockConstructorRegex);
+			if (!match?.groups) throw new Error(`Unknown modelId: ${fieldValue}`);
+			const {
+				groups: { modelId },
+			} = match;
+			console.warn(
+				`Unknown modelId: ${fieldName}, replacing with new FoundationModelIdentifier("${modelId}")`,
+			);
+			model = new FoundationModelIdentifier(modelId);
+		}
+
+		bedrockModels.push({
+			model,
+			isDeprecated,
+		});
+	}
+
+	return bedrockModels;
+};
