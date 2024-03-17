27a03e88b451bfa195b65caf3b064a8e1b372da8
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-03-17 09:46:30 +0100

    feat: cli (wip)

--- packages/cdk-sdk-versions-cli/src/cli.ts
+++ packages/cdk-sdk-versions-cli/src/cli.ts
@@ -1,18 +1,66 @@
 #!/usr/bin/env node
-import { runBedrock } from "cdk-sdk-versions-runner";
+import {
+	BedrockRunner,
+	KafkaRunner,
+	OpenSearchRunner,
+	SyntheticsRunner,
+} from "cdk-sdk-versions-runner";
+import type { Entries } from "type-fest";
 import yargs from "yargs";
+import { version } from "../package.json";
 
-console.log(runBedrock());
+const ALL_RUNNERS = "all";
+const runners = {
+	Bedrock: BedrockRunner,
+	Kafka: KafkaRunner,
+	OpenSearch: OpenSearchRunner,
+	Synthetics: SyntheticsRunner,
+};
 
-const argv = yargs(process.argv.slice(2))
-	.options({
-		a: { type: "boolean", default: false },
-		b: { type: "string", demandOption: true },
-		c: { type: "number", alias: "chill" },
-		d: { type: "array" },
-		e: { type: "count" },
-		f: { choices: ["1", "2", "3"] },
-	})
-	.parse();
+enum Command {
+	RUN = "run",
+}
 
-yargs.showHelp();
+interface YargsResult {
+	$0: "cdk-sdk-versions";
+	_: Command[];
+	runner: string;
+}
+
+const cli = yargs(process.argv.slice(2))
+	.scriptName("cdk-sdk-versions")
+	.demand(
+		1,
+		"You must provide a valid command, use --help to see available commands.",
+	)
+	.version(version)
+	.alias("v", "version")
+	.help("h")
+	.alias("h", "help");
+
+cli.command(`${Command.RUN} <runner>`, "Run a runner", (yargs) => {
+	yargs.positional("runner", {
+		type: "string",
+		default: ALL_RUNNERS,
+		demandOption: true,
+		describe: "Runner",
+		choices: [
+			ALL_RUNNERS,
+			...Object.keys(runners).map((runner) => runner.toLocaleLowerCase()),
+		],
+	});
+});
+
+for (const [name, runner] of Object.entries(runners) as Entries<
+	typeof runners
+>) {
+}
+
+const argv = cli.parse() as unknown as YargsResult;
+
+if (!argv._.includes(Command.RUN)) {
+	cli.showHelp();
+	process.exit(1);
+}
+
+console.log(argv.runner);
