3c0911d7ce9b9478a138671d15b5ddebe5802499
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-03-17 18:22:03 +0100

    feat: cli

--- packages/cdk-sdk-versions-cli/src/cli.ts
+++ packages/cdk-sdk-versions-cli/src/cli.ts
@@ -1,24 +1,67 @@
 #!/usr/bin/env node
 import {
 	BedrockRunner,
+	Ec2InstanceClassRunner,
+	Ec2InstanceSizeRunner,
+	Ec2WindowsVersionRunner,
 	KafkaRunner,
+	LinuxArmBuildImageRunner,
+	LinuxArmLambdaBuildImageRunner,
+	LinuxBuildImageRunner,
+	LinuxLambdaBuildImageRunner,
 	OpenSearchRunner,
+	RdsAuroraMysqlEngineRunner,
+	RdsAuroraPostgresEngineRunner,
+	RdsMariaDbEngineRunner,
+	RdsMySqlEngineRunner,
+	RdsOracleEngineRunner,
+	RdsPostgresEngineRunner,
+	RdsSqlServerEngineRunner,
 	SyntheticsRunner,
+	WindowsBuildImageRunner,
+	type CdkSdkVersionRunner,
 } from "cdk-sdk-versions-runner";
-import type { Entries } from "type-fest";
+import { Spinner } from "cli-spinner";
+import type { Class, Entries, Entry } from "type-fest";
 import yargs from "yargs";
 import { version } from "../package.json";
 
+type Runner = Class<CdkSdkVersionRunner<unknown, unknown>>;
+
 const ALL_RUNNERS = "all";
-const runners = {
-	Bedrock: BedrockRunner,
-	Kafka: KafkaRunner,
-	OpenSearch: OpenSearchRunner,
-	Synthetics: SyntheticsRunner,
+const runners: Record<string, Runner[]> = {
+	bedrock: [BedrockRunner],
+	kafka: [KafkaRunner],
+	opensearch: [OpenSearchRunner],
+	synthetics: [SyntheticsRunner],
+	codebuild: [
+		WindowsBuildImageRunner,
+		LinuxBuildImageRunner,
+		LinuxArmBuildImageRunner,
+		LinuxLambdaBuildImageRunner,
+		LinuxArmLambdaBuildImageRunner,
+	],
+	rds: [
+		RdsMySqlEngineRunner,
+		RdsMariaDbEngineRunner,
+		RdsPostgresEngineRunner,
+		RdsOracleEngineRunner,
+		RdsSqlServerEngineRunner,
+		RdsAuroraMysqlEngineRunner,
+		RdsAuroraPostgresEngineRunner,
+	],
+	ec2: [Ec2InstanceClassRunner, Ec2InstanceSizeRunner, Ec2WindowsVersionRunner],
 };
+
+type RunnerKey = keyof typeof runners;
+const runnerKeys = Object.keys(runners) as RunnerKey[];
 
 enum Command {
 	RUN = "run",
+}
+
+enum Option {
+	RUNNNER = "runner",
 }
 
 interface YargsResult {
@@ -36,31 +79,63 @@ const cli = yargs(process.argv.slice(2))
 	.version(version)
 	.alias("v", "version")
 	.help("h")
-	.alias("h", "help");
+	.alias("h", "help")
+	.showHelpOnFail(false);
 
-cli.command(`${Command.RUN} <runner>`, "Run a runner", (yargs) => {
-	yargs.positional("runner", {
-		type: "string",
-		default: ALL_RUNNERS,
-		demandOption: true,
-		describe: "Runner",
-		choices: [
-			ALL_RUNNERS,
-			...Object.keys(runners).map((runner) => runner.toLocaleLowerCase()),
-		],
-	});
+cli.command(`${Command.RUN} [${Option.RUNNNER}]`, "Run a runner", (yargs) => {
+	yargs
+		.positional(Option.RUNNNER, {
+			type: "string",
+			default: ALL_RUNNERS,
+			defaultDescription: "Run all runners",
+			describe: "Runner",
+			choices: [ALL_RUNNERS, ...runnerKeys],
+		})
+		.array(Option.RUNNNER);
 });
 
-for (const [name, runner] of Object.entries(runners) as Entries<
-	typeof runners
->) {
-}
-
 const argv = cli.parse() as unknown as YargsResult;
-
 if (!argv._.includes(Command.RUN)) {
-	cli.showHelp();
 	process.exit(1);
 }
 
-console.log(argv.runner);
+const run = async (argv: YargsResult) => {
+	const selectedRunnerKeys =
+		argv[Option.RUNNNER] === ALL_RUNNERS
+			? runnerKeys
+			: [argv[Option.RUNNNER] as RunnerKey];
+
+	const selectedRunners = Object.fromEntries(
+		selectedRunnerKeys.map<Entry<Record<RunnerKey, Runner[]>>>((key) => [
+			key,
+			runners[key],
+		]),
+	);
+
+	runnersExec: for (const [runnerKey, runnerClasses] of Object.entries(
+		selectedRunners,
+	) as Entries<typeof selectedRunners>) {
+		for (const runnerClass of runnerClasses) {
+			const spinner = new Spinner(`Running ${runnerClass.name}... %s`);
+			spinner.setSpinnerString("|/-\\");
+			spinner.start();
+
+			try {
+				const runner = new runnerClass();
+				const results = await runner.run();
+
+				spinner.stop(true);
+				console.log(`${runnerClass.name} results:\n`);
+				runner.consoleOutputResults(results);
+				console.log("\n");
+			} catch (error) {
+				spinner.stop(true);
+
+				console.error(`${runnerClass.name} error:\n`, error);
+				break runnersExec;
+			}
+		}
+	}
+};
+
+void run(argv);
