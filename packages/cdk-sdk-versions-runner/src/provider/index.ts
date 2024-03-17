250a7d687f5d20f79680e5469ac41bf94e043882
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-03-17 16:39:23 +0100

    fix: build export

--- packages/cdk-sdk-versions-runner/src/provider/index.ts
+++ packages/cdk-sdk-versions-runner/src/provider/index.ts
@@ -1,6 +1,6 @@
 export * from "./bedrock";
 export * from "./codebuild";
-export * from "./ec2";
+export * from "./ec2/";
 export * from "./eks";
 export * from "./kafka";
 // export * from "./lambda";
